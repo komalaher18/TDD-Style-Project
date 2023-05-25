2fc2b87638929c0550f0de524338c9cb7b17e2c5
Komal aher <komalaher18@gmail.com>
2023-05-25 15:34:02 -0700

    phase-3-constructor

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,11 @@
 class Person {
   // Your code here
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
 }
+
+
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,25 @@
 // Your code here
+const chai = require('chai');
+const expect = chai.expect;
+
+const Person = require('../problems/person');
+
+describe('Person', function(){
+    describe('constructor', function(){
+        it('should have name & age property', function(){
+            const name = "john";
+            const age = 20;
+            const result = new Person(name, age);
+            expect(result).to.have.property("name");
+            expect(result).to.have.property("age");
+        })
+        it('should set name & age property correctly', function(){
+            const name = "john";
+            const age = 20;
+            const result = new Person(name, age);
+            expect(result.name).to.equal("john");
+            expect(result.age).to.equal(20);
+        })
+})
+
+})
