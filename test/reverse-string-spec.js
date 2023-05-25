fd3f278331f5fb4a9e79235c7b5793bc0f4b14a7
Komal aher <komalaher18@gmail.com>
2023-05-24 17:39:13 -0700

    completed phase 1

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,15 @@
 // Your code here
+const chai = require('chai');
+const expect = chai.expect;
+
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString', function(){
+    it('should return a reverse string', function(){
+        const input = "fun";
+        const expected = "nuf";
+        const result = reverseString(input);
+        expect(result).to.equal(expected);
+    })
+
+})
